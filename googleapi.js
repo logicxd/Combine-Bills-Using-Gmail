642c152b43965e03cb49611f4e545f403fe64e04
David Moe <logicxd@gmail.com>
2020-12-27 21:05:56 -0800

    Add Google API authentication with custom refresh token

---
+++ googleapi.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const {google} = require('googleapis');
+const config = require('./config/config.development.json')
+
+function authenticateWithExistingRefreshToken() {
+    const oauth2Client = new google.auth.OAuth2(
+        config.client_id,
+        config.client_secret,
+        null
+    )
+    oauth2Client.setCredentials({
+        refresh_token: config.refresh_token
+    })
+    
+    google.options({
+        auth: oauth2Client
+    })
+}
+
+authenticateWithExistingRefreshToken()
+module.exports = {
+    google
+}
