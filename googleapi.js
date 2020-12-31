c398e1be3f08c156c2a501d88fd84668ec8f28a8
David Moe <logicxd@gmail.com>
2020-12-31 01:02:09 -0800

    Code cleanup and documentations

--- googleapi.js
+++ googleapi.js
@@ -6,8 +6,7 @@ const config = require('./config/config.development.json')
 function authenticateWithExistingRefreshToken() {
     const oauth2Client = new google.auth.OAuth2(
         config.client_id,
-        config.client_secret,
-        null
+        config.client_secret
     )
     oauth2Client.setCredentials({
         refresh_token: config.refresh_token
